d1aa21b91765cc53295e04e8488a4f9588ed31c3
imsagar0714 <aradhyashukla1817@gmail.com>
2026-04-20 17:07:37 +0530

    test mode

--- billing/urls.py
+++ billing/urls.py
@@ -4,11 +4,13 @@ from .views import (
     PlanListView,
     CreateSubscriptionView,
     RazorpayWebhookView,
+    VerifyPaymentView,   
 )
 
 urlpatterns = [
     path("current/", CurrentBillingView.as_view()),
     path("plans/", PlanListView.as_view()),
     path("subscribe/", CreateSubscriptionView.as_view()),
+    path("verify/", VerifyPaymentView.as_view()),   
     path("webhook/", RazorpayWebhookView.as_view()),
 ]
